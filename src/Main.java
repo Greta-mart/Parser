5105254241d1659f4ca35fd8bdad0ada914aeea1
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-11 17:27:46 +0300

    add comparing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,27 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Files f = new Files();
+
+        String path = "";
+        String countLine= "";
+        /*if (args.length ==2) {
+            try {
+                path = args[0];
+                countLine = args[1];
+            } catch (Exception ex) {
+                System.out.println(ex.getLocalizedMessage());
+            }
+            Files f = new Files(path, countLine);
+           // f.readFile();
+            f.compare();
+        }
+        else {
+            System.out.println("Please insert width and length after name of class");
+            System.exit(1);
+        }*/
+        Files f = new Files(path, countLine);
+        //f.readFile();
         f.readPath();
+        f.compare();
     }
 }
