0fc8e8871a1e57059c0e2f5f46832208f9051293
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-13 16:03:27 +0300

    add some fixes

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,35 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         String path = "";
-        String countLine= "";
+        String countLine = "";
         String lineForSearch = "";
         String lineForReplace = "";
-        /*if (args.length ==2) {
+
+        if (args.length == 2) {
             try {
                 path = args[0];
                 countLine = args[1];
             } catch (Exception ex) {
                 System.out.println(ex.getLocalizedMessage());
             }
-            Files f = new Files(path, countLine);
-           // f.readFile();
+            ParseFile f = new ParseFile(path, countLine, lineForSearch, lineForReplace);
             f.compare();
-        }
-        else {
-            System.out.println("Please insert width and length after name of class");
+        } else if (args.length == 3) {
+            try {
+                path = args[0];
+                lineForSearch = args[1];
+                lineForReplace = args[2];
+            } catch (Exception ex) {
+                System.out.println(ex.getLocalizedMessage());
+            }
+            ParseFile f = new ParseFile(path, countLine, lineForSearch, lineForReplace);
+            f.replacement();
+            System.out.println("File was successfully saved with replacement data");
+        } else {
+            System.out.println("Please insert path for file, line for search or line for search and than line for replace");
             System.exit(1);
-        }*/
-        ParseFile f = new ParseFile(path, countLine, lineForSearch, lineForReplace);
-        //f.readFile();
-        f.readPath();
-        System.out.println();
-       // f.compare();
-        f.replacement();
+        }
     }
 }
--- src/ParseFile.java
+++ src/ParseFile.java
@@ -1,19 +1,13 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class ParseFile {
     String path;
     String countLine;
     String lineForSearch;
     String lineForReplace;
-    Path fileName;
 
     public ParseFile(String path, String countLine, String lineForSearch, String lineForReplace) {
         this.path = path;
@@ -22,74 +16,60 @@ public class ParseFile {
         this.lineForReplace = lineForReplace;
     }
 
-    public void readPath() throws IOException {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Please enter the address of the file : ");
-        path = s.nextLine();
-        Path testFilePath = Paths.get(path);
-
-        fileName = testFilePath.getFileName();
-        System.out.println(fileName);
-
-        Path parent = testFilePath.getParent();
-        System.out.println(parent);
-
-      /*  System.out.print("Please enter the address of the line to count : ");
-        countLine = s.nextLine();
-*/
-        System.out.print("Please enter the address of the line for search : ");
-        lineForSearch = s.nextLine();
-
-        System.out.print("Please enter the address of the line for replacement : ");
-        lineForReplace = s.nextLine();
-
-        try (FileReader reader = new FileReader(path)) {
-            // читаем посимвольно и вывести - просто пробую работает ли
-            int c;
-            while ((c = reader.read()) != -1) {
-
-                System.out.print((char) c);
-            }
-        } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
-        }
-    }
-/*    public void readFile() {
-        try (FileReader reader = new FileReader(path)) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
-
     public void compare() {
+        int count = 0;
         try {
-            int count = 0;
             List<String> links1 = java.nio.file.Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
             for (String line : links1) {
                 if (line.equals(countLine)) {
                     count = count + 1;
                 }
             }
-            System.out.println(count);
+            System.out.println("The number of founded line: " + count);
         } catch (IOException e) {
             System.out.println(e);
         }
+        if (count == 0) {
+            System.out.println("The entered line isn't founded in this file");
+        }
     }
 
-    public void replacement() throws IOException {
+    public void replacement()  {
         StringBuilder sb = new StringBuilder();
-        List<String> links1 = java.nio.file.Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+        List<String> links1 = null;
+        try {
+            links1 = java.nio.file.Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         for (String line : links1) {
             if (line.equals(lineForSearch)) {
-                line.replaceAll(lineForSearch, lineForReplace);
-                FileWriter fileWriter = new FileWriter(String.valueOf(path));
+                int i = links1.indexOf(line);
+                links1.set(i, lineForReplace);
+            }
+        }
+        FileOutputStream output = null;
+        try {
+            try {
+                output = new FileOutputStream(path);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            final String LINE_SEPARATOR = System.getProperty("line.separator");
+            for (String line1 : links1) {
+                if (line1 != null) {
+                    output.write(line1.getBytes());
+                    output.write(LINE_SEPARATOR.getBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException ignore) {
+                }
             }
         }
     }
